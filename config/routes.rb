775b7e54d284079b2edec84895a67cf088777c62
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2013-08-03 23:23:14 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Dashing::Engine.routes.draw do
+  get "/" => "dashing#index"
+  get "/views/:widget" => "dashing#widget"
+  get "/events" => "dashing#events"
+  get "/:dashboard" => "dashing#dashboard"
 end
