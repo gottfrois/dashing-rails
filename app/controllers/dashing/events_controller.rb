b4f15f72e585cac99dcfc2c6b852f82a37770046
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2013-08-10 11:52:40 +0200

    fix: enable events stream

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -7,19 +7,19 @@ module Dashing
     respond_to :html
 
     def index
-      # begin
-      #   loop do
-      #     out = Dashing.histories.map do |id,event|
-      #       "data: #{event.to_json}\n\n"
-      #     end
-      #     response.stream.write out.join('')
-      #     sleep 1
-      #   end
-      # rescue IOError
-      #   Rails.logger.info 'Stream closed'
-      # ensure
-      #   response.stream.close
-      # end
+      begin
+        loop do
+          out = Dashing.histories.map do |id,event|
+            "data: #{event.to_json}\n\n"
+          end
+          response.stream.write out.join('')
+          sleep 1
+        end
+      rescue IOError
+        Rails.logger.info 'Stream closed'
+      ensure
+        response.stream.close
+      end
     end
 
     def create
