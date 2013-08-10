02ed7383af23724748ae253b7d3ec37495129a94
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2013-08-11 00:26:46 +0200

    fix: remove useless comments

--- lib/dashing/engine.rb
+++ lib/dashing/engine.rb
@@ -2,7 +2,6 @@ module Dashing
   class Engine < ::Rails::Engine
     isolate_namespace Dashing
 
-    # TODO add to asset pipeline
     config.assets.paths << Dashing::Engine.root.join('app', 'assets', 'fonts')
     config.assets.paths << Dashing::Engine.root.join('app', 'views', 'dashing')
     config.assets.paths << Dashing.config.view_path
