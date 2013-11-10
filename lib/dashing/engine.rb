56fda226f5979d3f05e18273fc3b3d9fc2be5930
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2013-11-10 17:32:08 +0100

    add font to asset pipeline

--- lib/dashing/engine.rb
+++ lib/dashing/engine.rb
@@ -8,6 +8,8 @@ module Dashing
     config.assets.paths.unshift       Dashing.config.widgets_js_path
     config.assets.paths.unshift       Dashing.config.widgets_css_path
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
     config.paths['app/views'].unshift Dashing::Engine.root.join('app', 'views', 'dashing', 'widgets')
   end
 end
