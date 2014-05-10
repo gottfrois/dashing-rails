f8edaf152a929336f7ee14b4b6224e15295ea6bc
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2014-05-10 18:29:45 +0400

    fix: redis child connection not closing. Fixes #28

--- lib/dashing/railtie.rb
+++ lib/dashing/railtie.rb
@@ -3,5 +3,16 @@ module Dashing
     initializer 'require dashing jobs' do
       Dir[Dashing.config.jobs_path.call.join('**', '*.rb')].each { |file| require file }
     end
+
+    initializer 'fix redis child connection' do
+      if defined?(::PhusionPassenger)
+        ::PhusionPassenger.on_event(:starting_worker_process) do |forked|
+          if forked
+            ::Dashing.redis.client.disconnect
+            ::Dashing.redis.client.connect
+          end
+        end
+      end
+    end
   end
 end
