ff18911ac280a57e17bc024c19889b561127b2c6
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2015-03-14 13:37:32 +0100

    remove useless respond_to to avoid adding responders gem for rails 4

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -2,8 +2,6 @@ module Dashing
   class EventsController < ApplicationController
     include ActionController::Live
 
-    respond_to :html
-
     def index
       response.headers['Content-Type']      = 'text/event-stream'
       response.headers['X-Accel-Buffering'] = 'no'
